f7b19bebc3bd9bd88db48a687a2dce9e2b918edc
Mess <45535471+Messier1071@users.noreply.github.com>
2024-09-26 19:01:25 -0300

    last

---
+++ ExercicioHeranca/classes.hpp
@@ -0,0 +1,65 @@
+#ifndef classescpp
+
+class Worker
+{
+private:
+    /* data */
+public:
+    Worker(/* args */);
+    ~Worker();
+};
+
+Worker::Worker(/* args */)
+{
+}
+
+Worker::~Worker()
+{
+}
+
+
+class Manager
+{
+private:
+    /* data */
+public:
+    Manager(/* args */);
+    ~Manager();
+};
+
+Manager::Manager(/* args */)
+{
+}
+
+Manager::~Manager()
+{
+}
+
+class Salesman
+{
+private:
+    /* data */
+public:
+    Salesman(/* args */);
+    ~Salesman();
+};
+
+Salesman::Salesman(/* args */)
+{
+}
+
+Salesman::~Salesman()
+{
+}
+
+
+
+
+
+
+
+
+
+
+
+#endif
